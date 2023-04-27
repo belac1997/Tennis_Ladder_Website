a973c07b82b383f7dfc80ca88a24493bfc3c937e
belac1997 <51928072+belac1997@users.noreply.github.com>
2023-04-27 01:32:19 -0400

    Add files via upload

---
+++ Data/TennisLadderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TennisLadder.Models;
+
+namespace TennisLadder.Data
+{
+    public class TennisLadderContext : DbContext
+    {
+        public TennisLadderContext (DbContextOptions<TennisLadderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TennisLadder.Models.Participant> Participants { get; set; } = default!;
+    }
+}
---
+++ Models/ParticipantContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+namespace TennisLadder.Models
+{
+    public class ParticipantContext : DbContext
+    {
+      public ParticipantContext(DbContextOptions<ParticipantContext> options) : base (options) { }
+        public DbSet<Participant> Participants { get; set; } = null!;
+    }
+    
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //    modelBuilder.Entity<Participant>().HasData(
+    //        new Participant { Id = 1, Name = "Maxwell Dee Anderson", Email = "maxwell@gg.com", Rank = 1, Wins = 5, Loses = 2, Availability = DayOfWeek.Monday, MatchPending = false }
+    //
+
+    //        );
+    //}
+}
